58babb62583b62c987c94c15ffb967b273c347c7
Piotr Halama <piotr.halama@sap.com>
2021-05-04 15:10:17 +0200

    Makefiles: use generic makefile (#83)
    * Makefiles: use generic makefile

---
+++ common/README.md
@@ -0,0 +1,3 @@
+# Common files
+
+This directory contains files commonly used in the repository.
---
+++ common/generic_makefile.mk
@@ -0,0 +1,31 @@
+# Generic makefile - contains most used targets
+# Required variables:
+# IMG - name of the image, with docker push repository, full path and tag
+# IMG_DOCKER_TAG - name of the image, with docker push repository, full path and DOCKER_TAG to satisfy Protescan requirements
+
+
+# Build the docker image
+.PHONY: docker-build
+docker-build:
+	docker build . -t ${IMG}
+
+# Push the docker image
+.PHONY: docker-push
+docker-push:
+	docker push ${IMG}
+	docker push ${IMG_DOCKER_TAG}
+
+# add second tag to the image
+.PHONY: ci-tag
+ci-tag:
+	docker tag ${IMG} ${IMG_DOCKER_TAG}
+
+# CI specified targets
+.PHONY: ci-pr
+ci-pr: docker-build ci-tag docker-push
+
+.PHONY: ci-main
+ci-main: docker-build docker-push
+
+.PHONY: ci-release
+ci-release: docker-build docker-push
